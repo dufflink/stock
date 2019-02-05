f8b0933e6d3992d2d5686ba4bda0c4e68e45db79
Maxim Skorynin <skoryninmaksim1@gmail.com>
2019-02-05 17:20:49 +0500

    Initial Commit

---
+++ stock-ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  stock-ios
+//
+//  Created by Maxim Skorynin on 05/02/2019.
+//  Copyright © 2019 Maxim Skorynin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
